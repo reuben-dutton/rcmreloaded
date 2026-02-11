1aa4cc00df67f4436cc2a2150ab870b9b830bd18
Reuben Dutton <24471347+reuben-dutton@users.noreply.github.com>
2026-02-11 20:35:04 +1000

    Added a hex_to_rgb util function

--- models/utils.py
+++ models/utils.py
@@ -12,3 +12,10 @@ def rgb_to_hex(rgb: tuple[int, int, int]):
 def rgb_to_name(rgb: tuple[int, int, int]):
     dist, ind = tree.query(rgb, k=1)
     return labels[ind]
+
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip('#')
+    if len(hex_color) != 6:
+        raise ValueError("Hex color must be 6 characters long")
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
