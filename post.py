af467d354c8e0911479f858e0e24479cee5900bd
Reuben Dutton <24471347+reuben-dutton@users.noreply.github.com>
2026-02-11 20:51:07 +1000

    Modified image alt text and generation probabilities

--- post.py
+++ post.py
@@ -8,46 +8,65 @@ if root not in sys.path:
 import io
 import math
 import os
+import random
 
 import atproto
 import dotenv
 import numpy as np
 
 from generators import RGBGenerator, HSVGenerator, CIELChGenerator
-from layers.constants import (
-    DEFAULT_SIZE,
-    DEFAULT_NONE_COLOUR,
+from generators.palettes import (
+    GradientPalette,
+    SinglePalette,
+    RandomPalette,
+    ComplementaryPalette,
+    AnalogousPalette,
+    TriadicPalette
 )
-from layers.canvas import CanvasLayer
-from layers.text import (
-    NameTextLayer,
-    HexcodeTextLayer,
-)
-from layers.space import SpaceIconLayer, SwatchLayer
-from layers.utils import compile_layers
-from models.colour import Colour
+from layers.frames import SingleFrame, HorizontalFrame, VerticalFrame, TwoByTwoFrame
 
 dotenv.load_dotenv()
-
 
 USERNAME = os.getenv('ATPROTO_CLIENT_USERNAME')
 PASSWORD = os.getenv('ATPROTO_CLIENT_PASSWORD')
 
 
-colour = CIELChGenerator().generate(1)
+generator = CIELChGenerator()
+options = {
+    1: {  # 1200 x 1200
+        'frames': [SingleFrame((1200, 1200))],
+        'palettes': [RandomPalette]
+    },
+    2: {  # 1200 x 1200
+        'frames': [HorizontalFrame((600, 1200)), VerticalFrame((1200, 600))],
+        'palettes': [RandomPalette, ComplementaryPalette]
+    },
+    3: {  # 1800 x 1800
+        'frames': [HorizontalFrame((600, 1800)), VerticalFrame((1800, 600))],
+        'palettes': [RandomPalette, GradientPalette, AnalogousPalette, TriadicPalette]
+    },
+    4: {  # 2400 x 2400, except the two-by-two, which is 1600 x 1600
+        'frames': [HorizontalFrame((600, 2400)), VerticalFrame((2400, 600)), TwoByTwoFrame((800, 800))],
+        'palettes': [RandomPalette, GradientPalette]
+    },
+}
 
 
-name_layer = NameTextLayer(text=colour.name.upper(), position=(1200 - 120, 1200 - 108*5/6 - 40), anchor='rb')
-hexcode_layer = HexcodeTextLayer(text=colour.hexcode, position=(1200 - 120, 1200 - 40), anchor='rb')
-canvas_layer = CanvasLayer(rgb=colour.rgb)
-icon_layer = SpaceIconLayer(position=(1200 - 84, 1200 - 160))
+n = np.random.choice(
+    [1, 2, 3, 4],
+    p=[0.25, 0.25, 0.30, 0.2]  # slightly more 3-colours, less 4-colours
+)
+frame = random.choice(options[n]['frames'])
+palette = random.choice(options[n]['palettes'])
+colours = palette(generator).generate(n)
+image = frame.construct_frame(colours)
 
-
-image = compile_layers(canvas_layer, name_layer, hexcode_layer)
 buffer = io.BytesIO()
 image.save(buffer, format="PNG")
 
 client = atproto.Client()
 client.login(USERNAME, PASSWORD)
 
-client.send_image("", image=buffer.getvalue(), image_alt = f"A picture of the color '{colour.name}'. In the centre of the image is the name and hex code of the color ({colour.hexcode})")
+alt_text = f"A picture of the following colors: {str([colour.name for colour in colours])}. Their hex codes are {str([colour.hexcode for colour in colours])}."
+
+client.send_image("", image=buffer.getvalue(), image_alt = alt_text)
