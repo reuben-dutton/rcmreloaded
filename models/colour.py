72ea9722a800142d547a8d392fb6f3f7f4197eb7
Reuben Dutton <24471347+reuben-dutton@users.noreply.github.com>
2026-02-11 20:35:33 +1000

    Colour objects can now be built from hex codes, and return rgb scaled from 0 to 1

--- models/colour.py
+++ models/colour.py
@@ -3,6 +3,7 @@ import dataclasses
 from models.utils import (
     rgb_to_hex,
     rgb_to_name,
+    hex_to_rgb,
 )
 
 
@@ -13,8 +14,21 @@ class Colour:
     name: str
     hexcode: str
 
+    @property
+    def rgb1(self) -> tuple[float, float, float]:
+        return (self.rgb[0] / 255, self.rgb[1] / 255, self.rgb[2] / 255)
+
     @classmethod
     def from_rgb(cls, rgb: tuple[int, int, int]):
+        return Colour(
+            rgb=rgb,
+            name=rgb_to_name(rgb),
+            hexcode=rgb_to_hex(rgb),
+        )
+
+    @classmethod
+    def from_hex(cls, hexcode: str):
+        rgb = hex_to_rgb(hexcode)
         return Colour(
             rgb=rgb,
             name=rgb_to_name(rgb),
